e5f3187893ff88acbf137f319fdb52c043616020
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-01-01 22:03:17 +0600

    Create 2.Reverse Integer.cpp

---
+++ 2.Reverse Integer.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        int answer = 0;
+        while (x != 0)
+        {
+            int lastdigit = x % 10;
+            if ((answer > INT_MAX / 10) || (answer < INT_MIN / 10))
+            {
+                return 0;
+            }
+
+            answer = answer * 10 + lastdigit;
+            x = x / 10;
+        }
+        return answer;
+    }
+};
